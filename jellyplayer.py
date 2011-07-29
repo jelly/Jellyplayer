45cccc6072d6208e4fecf6d798dcb8fcc6cf3302
Jelle van der Waa <jelle@vdwaa.nl>
2011-07-29 18:18:16 +0200

    Changed the whole code to use my custom json file instead of silly xml Signed-off-by: Jelle van der Waa <jelle@vdwaa.nl>

--- jellyplayer.py
+++ jellyplayer.py
@@ -1,71 +1,78 @@
-#!/usr/bin/python2
-from xml.dom import minidom
-import urwid
+#!/usr/bin/python2 
 
+import json
 from subprocess import call
 
-def getText(nodelist):
-    rc = []
-    for node in nodelist:
-        if node.nodeType == node.TEXT_NODE:
-            rc.append(node.data)
-    return ''.join(rc)
+def parseData(streams):
+    try:
+        fsock = open(streams, "r")
+        try:
+            data = json.load(fsock)
+        finally:
+            fsock.close()
+    except IOError:
+        print "no json file"
 
-def parseXML(xmlFile):
-    """
-    Parse the xml
-    """
- 
-    dom = minidom.parse('XBMC-Streams.xml')
-    channellist = {}
-    channels = dom.getElementsByTagName("item")
-    for channel in channels:
-        title = channel.getElementsByTagName("title")[0]
-        url = channel.getElementsByTagName("link")[0]
+    return data
 
-        channellist[getText(title.childNodes)] = getText(url.childNodes)
+
+
+def playVideo(stream):
+    if "rtmp" in stream:
+        # parse RTMP
+        stream_list = stream.split(" ")
+        swfurl = ""
+        playpath = ""
+        url = "" 
+        pageurl = ""
+        tcurl = ""
+        for a,b in enumerate(stream_list): 
+            if "rtmp" in b:
+                url = b
+            if "tcUrl" in b:
+                tcurl = " -t " + b.replace("tcUrl=","")
+            if "playpath" in b:
+                playpath = " -y " + b.replace("playpath=","")
+            if "Playpath" in b:
+                playpath = " -W " + b.replace("Playpath=","")
+            if "swfUrl" in b:
+                swfurl = " -W " + b.replace("swfUrl=","")
+            if "pageUrl" in b:
+                pageurl = " -p " + b.replace("pageUrl=","")
+
+        rtmpdump = "rtmpdump -r " + url + playpath + swfurl + pageurl
+        if "live=true" in stream_list:
+            rtmpdump += " -v "
+        rtmpdump += " | mplayer -cache 2000 -"
+        print rtmpdump
+        call(rtmpdump, shell=True)
         
-        #print  getText(title.childNodes) + " " + getText(url.childNodes)
-    return channellist
+    else:
+        if "http" in stream:
+            call("mplayer -cache 2000 -playlist " + stream, shell=True)
+        else:
+            call("mplayer -cache 2000 " + stream, shell=True)
 
-def playVideo(video):
-    video_list = video.split(" ")
-    url = video_list[0] # -r
-    playpath = video_list[1].replace("playpath=","") # -y
-    swfurl = video_list[2].replace("swfUrl=","") # -W
+def mainloop(data):
+    bar = 1
 
-    rtmpdump = "rtmpdump -r " + url + " -y " + playpath + " -W " + swfurl 
-    if "live=true" in video_list:
-        rtmpdump += " -v "
-    rtmpdump += " | mplayer -cache 2000 -"
-    #os.system(rtmpdump)
-    call(rtmpdump, shell=True)
+    while(bar != 0):
 
-
-
-
-if __name__ == "__main__":
-    channellist  = parseXML("XBMC-Streams.xml")
-
-    bar = 1 
-    while bar != 0:
         foo = 1
-        for i in channellist:
-            print "%i. %s" % (foo, i)
+        for d in data:
+            print "%i. %s " % (foo,d['title'])
             foo += 1
 
         print "press '0' for quit"
         bar=input('Please enter a value:')
-
         if bar != 0:
             foo = 1
-            for i in channellist:
+            for d in data:
                 if foo == bar:
-                        playVideo(channellist[i])
+                    playVideo(d['stream'])
                 foo += 1
 
 
-    #bbc = channellist["BBC News"]
-    #playVideo(bbc)
 
-
+if __name__ == "__main__":
+    mainloop(parseData('streams.json'))
